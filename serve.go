14ac0ee0a0c60cb3235178b9c6269da6aca72e08
sentriz <senan@senan.xyz>
2021-05-17 20:58:44 +0100

    only show useful interface addresses

--- serve.go
+++ serve.go
@@ -5,20 +5,29 @@ import (
 	"net"
 	"net/http"
 	"os"
+	"regexp"
 )
+
+var commonIPv4 = regexp.MustCompile(`^(10|192|127)\.`)
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("please provide a listen address")
 	}
-	addrs, _ := net.InterfaceAddrs()
-	for _, address := range addrs {
-		log.Printf("have address %s", address)
+
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatalf("error listing interfaces: %v", err)
 	}
+	for _, address := range addrs {
+		if commonIPv4.MatchString(address.String()) {
+			log.Printf("have address %s", address)
+		}
+	}
+
 	files := http.FileServer(http.Dir("."))
-	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	log.Fatal(http.ListenAndServe(os.Args[1], http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("%s requests %q", r.RemoteAddr, r.URL)
 		files.ServeHTTP(w, r)
-	})
-	log.Fatal(http.ListenAndServe(os.Args[1], handler))
+	})))
 }
