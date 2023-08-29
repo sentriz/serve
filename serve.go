34803c54b8aa1f712768c0776a2fd4a2b471e879
sentriz <senan@senan.xyz>
2023-08-29 09:57:27 +0100

    a

--- serve.go
+++ serve.go
@@ -2,27 +2,13 @@ package main
 
 import (
 	"log"
-	"net"
 	"net/http"
 	"os"
-	"regexp"
 )
-
-var commonIPv4 = regexp.MustCompile(`^(10|192|127)\.`)
 
 func main() {
 	if len(os.Args) != 2 {
 		log.Fatal("please provide a listen address")
-	}
-
-	addrs, err := net.InterfaceAddrs()
-	if err != nil {
-		log.Fatalf("error listing interfaces: %v", err)
-	}
-	for _, address := range addrs {
-		if commonIPv4.MatchString(address.String()) {
-			log.Printf("have address %s", address)
-		}
 	}
 
 	files := http.FileServer(http.Dir("."))
